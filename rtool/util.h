32bd78076237f4a7be7a24e05318cd513f34fdef
Vincent Yanzee J. Tan <vytdev@gmail.com>
2024-12-24 07:51:31 +0800

    rm: Separate the rvm toolchain from the vm

--- rtool/util.h
+++
@@ -1,9 +0,0 @@
-#ifndef RTOOL_UTIL_H_
-#define RTOOL_UTIL_H_
-#include "../rvm/util.h"
-#include "../rvm/config.h"
-
-/* Reads an ascii file. */
-char *util_readtext(char *path, uvar *size);
-
-#endif // RTOOL_UTIL_H_
